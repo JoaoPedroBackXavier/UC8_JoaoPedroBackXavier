19cfbfaeea7d6ceb5eb049541d0a8a42183bb100
JOAO PEDRO BACK XAVIER <04821428040@senacrs.edu.br>
2025-03-07 17:17:15 -0300

    aula de jframe 2

---
+++ testesInterfaceGrafica(aula2)/src/main/java/com/mycompany/mavenproject1/primeiroDesign.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author JOAOPEDROBACKXAVIER
+ */
+public class primeiroDesign {
+
+    public static void main(String[] args) {
+        
+        
+        
+    }
+}
